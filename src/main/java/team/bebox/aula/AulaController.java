d36b06dcbf1b0f5a12e2f19cf0d0329d9194f0bb
felipe <felipegabriel@rocketmail.com>
2020-07-14 23:11:29 -0300

    Corrigido bug

--- src/main/java/team/bebox/aula/AulaController.java
+++ src/main/java/team/bebox/aula/AulaController.java
@@ -67,9 +67,6 @@ public class AulaController {
             }
             aulas = aulaServiceImpl.buscaPorAlunoId(aluno.getId());
         }
-		if(aluno.getAulaChecked()!=null) {
-			aulas = aulaServiceImpl.buscaPorAlunoId(idAluno);
-		}
 		return new AulaResponse(aluno, aulas);
 	}
 	
